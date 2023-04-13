2f6569c685149cd72bce4c81f66c35f91d5615bd
mohammad ghane <mghaneh1375@yahoo.com>
2023-04-13 22:43:53 +0430

    ok

---
+++ database/migrations/2023_04_13_181047_change_foreign_const_in_vides_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('video', function (Blueprint $table) {
+            $table->dropForeign(['cat_id']);
+            $table->foreign('cat_id')->references('id')->on('video')->onUpdate('cascade')->onDelete('cascade');
+        });
+        
+        Schema::table('gallery', function (Blueprint $table) {
+            $table->dropForeign(['cat_id']);
+            $table->foreign('cat_id')->references('id')->on('gallery')->onUpdate('cascade')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vides', function (Blueprint $table) {
+            //
+        });
+    }
+};
